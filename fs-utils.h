19dddf8fcafe85c8b4798c6bdd364ef3f7a43bd8
Antti Kantee <pooka@iki.fi>
2013-04-06 11:44:28 +0000

    __BEGIN/END_DECLS

--- fs-utils.h
+++ fs-utils.h
@@ -7,6 +7,16 @@
 #include <sys/cdefs.h>
 #endif
 
+#ifndef __BEGIN_DECLS
+# ifdef __cplusplus
+#  define __BEGIN_DECLS extern "C" {
+#  define __END_DECLS }
+# else
+#  define __BEGIN_DECLS
+#  define __END_DECLS
+# endif
+#endif
+
 #include <sys/types.h>
 
 #endif /* FSUTILS_H_ */
