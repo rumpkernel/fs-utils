1f21d0347843167f6d2050899ec22721bad1c2a6
Antti Kantee <pooka@iki.fi>
2013-04-21 22:19:31 +0200

    remove compat already handled by fs-utils.h

--- src/find_main.c
+++ src/find_main.c
@@ -68,10 +68,6 @@ __RCSID("$NetBSD: main.c,v 1.31 2013/01/24 17:50:08 christos Exp $");
 #define	REG_EXTENDED	1
 #endif
 
-#ifdef __linux__
-#define __dead
-#endif
-
 
 time_t now;			/* time find was run */
 int dotfd;			/* starting directory */
