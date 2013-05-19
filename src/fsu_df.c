ccd23943939ae99be9b7f332e4e23e4e04768140
Antti Kantee <pooka@iki.fi>
2013-05-19 12:48:14 +0000

    Hugely simplify with the aim to further improve portability.
    Since we're going to have only one file system mounted, we can get
    rid of half of the code.

--- src/fsu_df.c
+++ src/fsu_df.c
@@ -57,9 +57,6 @@
 
 #include "fsu_compat.h"
 
-/* from sys/mount.h */
-#define	MNAMELEN    90
-
 /* from sys/statvfs.h */
 #define	NB_VFS_NAMELEN	32
 #define	NB_VFS_MNAMELEN	1024
@@ -69,16 +66,16 @@ struct nbstatvfs {
 	unsigned long	f_frsize;
 	unsigned long	f_iosize;
 
-	fsblkcnt_t	f_blocks;
+	uint64_t	f_blocks;
 
-	fsblkcnt_t	f_bfree;
-	fsblkcnt_t	f_bavail;
-	fsblkcnt_t	f_bresvd;
+	uint64_t	f_bfree;
+	uint64_t	f_bavail;
+	uint64_t	f_bresvd;
 
-	fsfilcnt_t	f_files;
-	fsfilcnt_t	f_ffree;
-	fsfilcnt_t	f_favail;
-	fsfilcnt_t	f_fresvd;
+	uint64_t	f_files;
+	uint64_t	f_ffree;
+	uint64_t	f_favail;
+	uint64_t	f_fresvd;
 
 	uint64_t  	f_syncreads;
 	uint64_t  	f_syncwrites;
@@ -89,7 +86,7 @@ struct nbstatvfs {
 	uint32_t	f_fsidx;
 	unsigned long	f_fsid;
 	unsigned long	f_namemax;
-	uid_t		f_owner;
+	uint32_t	f_owner;
 
 	uint32_t	f_spare[4];
 
@@ -98,11 +95,7 @@ struct nbstatvfs {
 	char	f_mntfromname[NB_VFS_MNAMELEN];
 };
 
-static char	*getmntpt(const char *);
 static void	 prtstat(struct nbstatvfs *, int);
-static int	 selected(const char *, size_t);
-static void	 maketypelist(char *);
-static size_t	 regetmntinfo(struct nbstatvfs **, size_t);
 __dead static void usage(void);
 static void	 prthumanval(int64_t, const char *);
 static void	 prthuman(struct nbstatvfs *, int64_t, int64_t);
@@ -112,45 +105,10 @@ static long	 usize;
 static char	**typelist;
 extern int rump_i_know_what_i_am_doing_with_sysents;
 
-/* from libc/gen/getmntinfo.c */
-static int
-getmntinfo(struct nbstatvfs **mntbufp, int flags)
-{
-	static struct nbstatvfs *mntbuf = NULL;
-	static int mntsize = 0;
-	static size_t bufsize = 0;
-	int rv;
-	//_DIAGASSERT(mntbufp != NULL);
-
-	bufsize = sizeof(struct nbstatvfs);
-	mntbuf = malloc(bufsize);
-
-	if (mntsize <= 0 &&
-	    (mntsize = rump_sys_getvfsstat(NULL, (size_t)0, RUMP_MNT_NOWAIT)) == -1)
-		return (0);
-	if (bufsize > 0 &&
-	    (mntsize = rump_sys_getvfsstat((struct statvfs *)mntbuf, bufsize, flags)) == -1)
-		return (0);
-	while (bufsize <= mntsize * sizeof(struct nbstatvfs)) {
-		if (mntbuf)
-			free(mntbuf);
-		bufsize = (mntsize + 1) * sizeof(struct nbstatvfs);
-		if ((mntbuf = malloc(bufsize)) == NULL)
-			return (0);
-		if ((mntsize = rump_sys_getvfsstat((struct statvfs *)mntbuf, bufsize,
-						flags)) == -1)
-			return (0);
-	}
-	*mntbufp = mntbuf;
-	return (mntsize);
-}
-
 int
 main(int argc, char *argv[])
 {
-	struct stat stbuf;
-	struct nbstatvfs *mntbuf;
-	long mntsize;
+	struct nbstatvfs mntbuf;
 	int ch, i, maxwidth, width;
 	char *mntpt;
 
@@ -200,10 +158,7 @@ main(int argc, char *argv[])
 			Pflag = 1;
 			break;
 		case 't':
-			if (typelist != NULL)
-				errx(EXIT_FAILURE,
-				    "only one -t option may be specified.");
-			maketypelist(optarg);
+			/* ignored */
 			break;
 		case '?':
 		default:
@@ -227,174 +182,11 @@ main(int argc, char *argv[])
 	argc -= optind;
 	argv += optind;
 
-	mntsize = getmntinfo(&mntbuf, RUMP_MNT_NOWAIT);
-	if (mntsize == 0)
-		err(EXIT_FAILURE,
-		    "retrieving information on mounted file systems");
-
-	if (*argv == NULL) {
-		mntsize = regetmntinfo(&mntbuf, mntsize);
-	} else {
-		if ((mntbuf = malloc(argc * sizeof(*mntbuf))) == NULL)
-			err(EXIT_FAILURE, "can't allocate statvfs array");
-		mntsize = 0;
-		for (/*EMPTY*/; *argv != NULL; argv++) {
-			if (rump_sys_stat(*argv, &stbuf) < 0) {
-				if ((mntpt = getmntpt(*argv)) == 0) {
-					warn("%s", *argv);
-					continue;
-				}
-			} else if (S_ISBLK(stbuf.st_mode)) {
-				if ((mntpt = getmntpt(*argv)) == 0)
-					mntpt = *argv;
-			} else
-				mntpt = *argv;
-			/*
-			 * Statfs does not take a `wait' flag, so we cannot
-			 * implement nflag here.
-			 */
-			if (!rump_sys_statvfs1(mntpt, (struct statvfs *)&mntbuf[mntsize],
-						RUMP_MNT_WAIT))
-				if (lflag &&
-				    (mntbuf[mntsize].f_flag & RUMP_MNT_LOCAL) == 0)
-					warnx("Warning: %s is not a local %s",
-					    *argv, "file system");
-				else if
-				    (!selected(mntbuf[mntsize].f_fstypename,
-					sizeof(mntbuf[mntsize].f_fstypename)))
-					warnx("Warning: %s mounted as a %s %s",
-					    *argv,
-					    mntbuf[mntsize].f_fstypename,
-					    "file system");
-				else
-					++mntsize;
-			else
-				warn("%s", *argv);
-		}
-	}
-
-	maxwidth = 0;
-	for (i = 0; i < mntsize; i++) {
-		width = (int)strlen(mntbuf[i].f_mntfromname);
-		if (width > maxwidth)
-			maxwidth = width;
-	}
-	for (i = 0; i < mntsize; i++)
-		prtstat(&mntbuf[i], maxwidth);
+	if (rump_sys_statvfs1("/",
+	    (struct statvfs *)&mntbuf, RUMP_MNT_WAIT) == -1)
+		errx(1, "statvfs failed: %d", errno);
+	prtstat(&mntbuf, maxwidth);
 	return 0;
-}
-
-static char *
-getmntpt(const char *name)
-{
-	size_t mntsize, i;
-	struct nbstatvfs *mntbuf;
-
-	mntsize = getmntinfo(&mntbuf, RUMP_MNT_NOWAIT);
-	if (mntsize == 0)
-		err(EXIT_FAILURE, "Can't get mount information");
-	for (i = 0; i < mntsize; i++) {
-		if (!strcmp(mntbuf[i].f_mntfromname, name))
-			return mntbuf[i].f_mntonname;
-	}
-	return 0;
-}
-
-static enum { IN_LIST, NOT_IN_LIST } which;
-
-static int
-selected(const char *type, size_t len)
-{
-	char **av;
-
-	/* If no type specified, it's always selected. */
-	if (typelist == NULL)
-		return 1;
-	for (av = typelist; *av != NULL; ++av)
-		if (!strncmp(type, *av, len))
-			return which == IN_LIST ? 1 : 0;
-	return which == IN_LIST ? 0 : 1;
-}
-
-static void
-maketypelist(char *fslist)
-{
-	size_t i;
-	char *nextcp, **av;
-
-	if ((fslist == NULL) || (fslist[0] == '\0'))
-		errx(EXIT_FAILURE, "empty type list");
-
-	/*
-	 * XXX
-	 * Note: the syntax is "noxxx,yyy" for no xxx's and
-	 * no yyy's, not the more intuitive "noyyy,noyyy".
-	 */
-	if (fslist[0] == 'n' && fslist[1] == 'o') {
-		fslist += 2;
-		which = NOT_IN_LIST;
-	} else
-		which = IN_LIST;
-
-	/* Count the number of types. */
-	for (i = 1, nextcp = fslist;
-	    (nextcp = strchr(nextcp, ',')) != NULL; i++)
-		++nextcp;
-
-	/* Build an array of that many types. */
-	if ((av = typelist = malloc((i + 1) * sizeof(*av))) == NULL)
-		err(EXIT_FAILURE, "can't allocate type array");
-	av[0] = fslist;
-	for (i = 1, nextcp = fslist;
-	    (nextcp = strchr(nextcp, ',')) != NULL; i++) {
-		*nextcp = '\0';
-		av[i] = ++nextcp;
-	}
-	/* Terminate the array. */
-	av[i] = NULL;
-}
-
-/*
- * Make a pass over the filesystem info in ``mntbuf'' filtering out
- * filesystem types not in ``fsmask'' and possibly re-stating to get
- * current (not cached) info.  Returns the new count of valid statvfs bufs.
- */
-static size_t
-regetmntinfo(struct nbstatvfs **mntbufp, size_t mntsize)
-{
-	size_t i, j;
-	struct nbstatvfs *mntbuf;
-
-	if (!lflag && typelist == NULL && aflag)
-		return nflag ? mntsize : (size_t)getmntinfo(mntbufp, RUMP_MNT_WAIT);
-
-	mntbuf = *mntbufp;
-	j = 0;
-	for (i = 0; i < mntsize; i++) {
-		if (!aflag && (mntbuf[i].f_flag & RUMP_MNT_IGNORE) != 0)
-			continue;
-		if (lflag && (mntbuf[i].f_flag & RUMP_MNT_LOCAL) == 0)
-			continue;
-		if (!selected(mntbuf[i].f_fstypename,
-		    sizeof(mntbuf[i].f_fstypename)))
-			continue;
-		if (nflag)
-			mntbuf[j] = mntbuf[i];
-		else {
-			struct nbstatvfs layerbuf = mntbuf[i];
-			(void)rump_sys_statvfs1(mntbuf[i].f_mntonname,
-					(struct statvfs *)&mntbuf[j], RUMP_MNT_WAIT);
-			/*
-			 * If the FS name changed, then new data is for
-			 * a different layer and we don't want it.
-			 */
-			if (memcmp(layerbuf.f_mntfromname,
-			    mntbuf[j].f_mntfromname, MNAMELEN))
-				mntbuf[j] = layerbuf;
-		}
-		j++;
-	}
-	return j;
 }
 
 static void
