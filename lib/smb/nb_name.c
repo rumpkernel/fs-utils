07001e9bfaca9796e31e49ffbeb1220d19cc2b64
Arnaud Ysmal <stacktic@NetBSD.org>
2013-12-13 19:35:59 +0100

    Make sure we keep enough space to encode the type

--- lib/smb/nb_name.c
+++ lib/smb/nb_name.c
@@ -168,7 +168,7 @@ nb_name_encode(struct nb_name *np, u_char *dst)
 		memsetw(cp + 2, NB_NAMELEN - 1, NBENCODE(' '));
 		cp += NB_ENCNAMELEN;
 	} else {
-		for (i = 0; *name && i < NB_NAMELEN; i++, cp += 2, name++) {
+		for (i = 0; *name && i < NB_NAMELEN - 1; i++, cp += 2, name++) {
 			ch = NBENCODE(toupper(*name));
 			memcpy(cp, &ch, 2);
 		}
